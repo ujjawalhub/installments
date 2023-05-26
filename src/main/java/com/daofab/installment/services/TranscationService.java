8ca51a54f43f16ff6accbb2544b75e74e3eb3e9d
Ujjawal Jaiswal <ujjawal@Ujjawals-MacBook-Pro.local>
2023-05-26 18:35:36 +0530

    logger

--- src/main/java/com/daofab/installment/services/TranscationService.java
+++ src/main/java/com/daofab/installment/services/TranscationService.java
@@ -9,6 +9,7 @@ import com.daofab.installment.model.ChildData;
 import com.daofab.installment.model.ParentData;
 import com.daofab.installment.utils.ControllerResponse;
 import com.daofab.installment.utils.ResponseUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -20,6 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
+@Slf4j
 public class TranscationService {
 
     @Autowired
@@ -38,6 +40,7 @@ public class TranscationService {
     public ControllerResponse getParentData(Integer page, Integer size) {
         Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Order.asc("id")).ascending());
         Page<Parent> parentPageData = parentDao.findAllByPageable(pageable);
+        log.info(parentPageData.toString());
         return ResponseUtils.createPageResponse(parentPageData, parentMapper.map(parentPageData.getContent()),
                 "Parent data not found");
     }
@@ -45,6 +48,7 @@ public class TranscationService {
     @Transactional
     public ControllerResponse getChildData(Integer parentId) {
         List<ChildData> childDataList = childMapper.map(childDao.findAllByParentId(parentId));
+        log.info(childDataList.toString());
         return ResponseUtils.createDataResponse(childDataList, "Child not found for requested parent Id");
     }
 
